aaa86ff553dddddd0c5cd0fd3f224ca3b98e2b75
Oleksandr_Kobzar <oleksandrkobzar@gmail.com>
2026-01-30 16:55:19 +0200

    Update

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,20 @@
 'use strict';
 class Storage {
+  #items;
+
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
   getItems() {
-    return this.items;
+    return this.#items;
   }
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
   removeItem(itemToRemove) {
-    const index = this.items.indexOf(itemToRemove);
+    const index = this.#items.indexOf(itemToRemove);
     if (index !== -1) {
-      this.items.splice(index, 1);
+      this.#items.splice(index, 1);
     }
   }
 }
